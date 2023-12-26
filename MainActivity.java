862403e29c3b667086fce4b86386bbb39db1f537
harshilpaghdar <129817001+harshilpaghdar@users.noreply.github.com>
2023-12-26 11:26:02 +0530

    1st lab program

--- MainActivity.java
+++ MainActivity.java
@@ -1,42 +1,14 @@
-package com.example.demo;
+package com.example.a1stlab;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Toast;
+
 import android.os.Bundle;
-import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Toast.makeText( this, "OnStart",Toast.LENGTH_SHORT).show();    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Toast.makeText( this, "OnResume",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Toast.makeText( this, "OnPause",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onStop(){
-        super.onStop();
-       Toast.makeText( this, "OnStop",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Toast.makeText( this, "OnDestroy",Toast.LENGTH_SHORT).show();
-    }
-    private void showMessage(String message){
-        Log.d("Lifecycle",message);
-        Toast.makeText( this, "message",Toast.LENGTH_SHORT).show();
     }
 }
