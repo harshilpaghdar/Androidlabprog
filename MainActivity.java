949f136bc4fb9b5ce41f1ee2e8a8671d8b204919
harshilpaghdar <129817001+harshilpaghdar@users.noreply.github.com>
2024-01-04 11:25:47 +0530

    lab4 create a responsive button

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,25 @@
-package com.example.a1stlab;
+package com.example.practical4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        EditText editTextName=findViewById(R.id.editTextName);
+        Button buttonSubmit=findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            Toast.makeTest(MainActivity.this,"hello", Toast.LENGTH_SHORT).show();
+            }
+    });
+}
 }
